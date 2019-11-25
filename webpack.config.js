90312d8f9bb869648b8c6346d07d975efaac893d
Yaroslav Harmash <feroxes@gmail.com>
2019-11-25 16:13:41 +0200

    improvement: set up eslint, fixed errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,62 @@
 const path = require('path');
 const MiniSccExtractPlugin = require('mini-css-extract-plugin');
-module.exports = {entry: {
-  app: './src/index.js',}, output: {
-  filename: '[name].js',
+
+module.exports = {
+  entry: { app: './src/index.js' },
+  output: {
+    filename: '[name].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist'},
+    publicPath: '/dist',
+  },
   module: {
-  rules: [
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/',
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        MiniSccExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        },
-        {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: {path: 'src/config/postcss.config.js'} }
-        },
-        {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
-        }
-      ]
-    },
-    {
-      test: /\.css$/,
-      use: [
-        MiniSccExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        },
-        {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: {path: 'src/config/postcss.config.js'} }
-        },
-      ]
-    }
-    ]
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniSccExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: 'src/config/postcss.config.js' } },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniSccExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: 'src/config/postcss.config.js' } },
+          },
+        ],
+      },
+    ],
   },
   devServer: {
-  overlay: true,
+    overlay: true,
   },
   plugins: [
     new MiniSccExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
